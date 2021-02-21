9c417f9f167405ba1561afbaacc54f824ecb8ca6
LavanSumanan <lavan.sumanan@gmail.com>
2021-02-21 05:34:52 -0500

    Added animations to cloud

--- heartRateSensor/heartRateSensor.ino
+++ heartRateSensor/heartRateSensor.ino
@@ -1,21 +1,14 @@
-#include <Keyboard.h>
-
 const int sensorPin = A1;
 
 int sensorValue;
 int lastSensorValue = LOW;
-
-char leftCtrlKey = KEY_LEFT_CTRL;
-char leftShift = KEY_LEFT_SHIFT;
-char rightCtrlKey = KEY_RIGHT_CTRL;
-char rightShift = KEY_RIGHT_SHIFT;
 
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 5000;
 
 void setup() {
   pinMode(sensorPin, INPUT);
-  Keyboard.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -25,16 +18,11 @@ void loop() {
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (abs(reading - buttonState) > 40) {
-      Keyboard.press(leftCtrlKey);
-      Keyboard.press(rightCtrlKey);
-      Keyboard.press(leftShift);
-      Keyboard.press(rightShift);
-      Keyboard.press('b');
-      delay(100);
-      Keyboard.releaseAll();
-      delay(10000);
+    if (abs(reading - sensorValue) > 40) {
+      // put stuff here
+      Serial.println("initalize breeve");
+      delay(1000);
     }
   }
   lastSensorValue = reading;
-} 
+}
